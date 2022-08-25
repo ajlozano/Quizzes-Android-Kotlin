c2e546ddb6257bb8f268e9fa996b7cf5e01ba1ee
Toni <ajlozano@users.noreply.github.com>
2022-08-25 15:18:35 +0200

    Added constants on Question class and implemented radioGroup and button listeners.

--- app/src/main/java/uoc/quizz/MainActivity.kt
+++ app/src/main/java/uoc/quizz/MainActivity.kt
@@ -1,23 +1,48 @@
 package uoc.quizz
 
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
 import com.squareup.picasso.Picasso
 import uoc.quizz.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+    var questionNum = 0
+    var questions = arrayListOf(
+        Question(QConstants.Q1_TITLE, arrayOf(QConstants.Q1_OPT_A, QConstants.Q1_OPT_B, QConstants.Q1_OPT_C), QConstants.Q1_OPT_B, QConstants.Q1_IMG_URL),
+        Question(QConstants.Q2_TITLE, arrayOf(QConstants.Q2_OPT_A, QConstants.Q2_OPT_B, QConstants.Q2_OPT_C), QConstants.Q2_OPT_B, QConstants.Q2_IMG_URL),
+        Question(QConstants.Q3_TITLE, arrayOf(QConstants.Q3_OPT_A, QConstants.Q3_OPT_B, QConstants.Q3_OPT_C), QConstants.Q3_OPT_B, QConstants.Q3_IMG_URL)
+    )
+    var selectedOption : String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Array of questions for the quizz
-        //val questions = Array<Question>(5)
-        //questions.set(0, Question("prueba", arrayOf("prueba1","prueba2", "prueba3"), "prueba", binding.image))
-
         Picasso.get()
-            .load("https://upload.wikimedia.org/wikipedia/commons/e/ed/Scapula_ant_numbered.png")
+            .load(questions[questionNum].imgURL)
             .into(binding.image)
+
+        binding.radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when(checkedId){
+                binding.optionOne.id -> selectedOption = binding.optionOne.text as String
+                binding.optionTwo.id -> selectedOption = binding.optionOne.text as String
+                binding.optionThree.id -> selectedOption = binding.optionOne.text as String
+                else -> selectedOption = "No option selected"
+            }
+        }
+
+        binding.button.setOnClickListener(View.OnClickListener { _ ->
+            Toast.makeText(this, "${binding.radioGroup.checkedRadioButtonId}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, selectedOption, Toast.LENGTH_SHORT).show()
+        })
+
     }
 }
+
 
--- app/src/main/java/uoc/quizz/Question.kt
+++ app/src/main/java/uoc/quizz/Question.kt
@@ -2,6 +2,28 @@ package uoc.quizz
 
 import android.widget.ImageView
 
-public class Question (val title:String, val options:Array<String>, val result:String, var image:ImageView) {
+
+public object QConstants {
+    const val Q1_TITLE = "pregunta1"
+    const val Q1_OPT_A = "A"
+    const val Q1_OPT_B = "B"
+    const val Q1_OPT_C = "C"
+    const val Q1_IMG_URL = "https://upload.wikimedia.org/wikipedia/commons/e/ed/Scapula_ant_numbered.png"
+
+    const val Q2_TITLE = ""
+    const val Q2_OPT_A = ""
+    const val Q2_OPT_B = ""
+    const val Q2_OPT_C = ""
+    const val Q2_IMG_URL = "https://upload.wikimedia.org/wikipedia/commons/e/ed/Scapula_ant_numbered.png"
+
+    const val Q3_TITLE = ""
+    const val Q3_OPT_A = ""
+    const val Q3_OPT_B = ""
+    const val Q3_OPT_C = ""
+    const val Q3_IMG_URL = "https://upload.wikimedia.org/wikipedia/commons/e/ed/Scapula_ant_numbered.png"
+
+}
+
+public class Question (val title:String, val options:Array<String>, val result:String, val imgURL:String) {
 
 }
